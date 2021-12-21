545c94273cd2a54af4f44c0413543a5d5f7bce3f
Edgar CORTES <edgar.cor.hdz@gmail.com>
2021-12-20 20:16:15 -0600

    titres des livres de la 1ère page dans une liste + if pour savoir s'il y a un next

--- infoCategorie.py
+++ infoCategorie.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
+page = 'http://books.toscrape.com/catalogue/category/books/mystery_3/page-1.html'
 reponse = requests.get(page)
 
 if reponse.ok:
@@ -10,6 +10,26 @@ if reponse.ok:
 
     print(soup)
 
+    ###  titre des livres dans une liste  ###
+    h3 = soup.find_all('h3')
+    # print(h3)
+    # print(len(h3))
+    titre = []
+
+    for i in range(0, len(h3)):
+        titre.append(h3[i].find_next('a')['title'])  # "h3" contient "a", dont l'attribut "title" va dans une liste vide
+
+    print(titre)
+    pager = soup.findAll('li')
+    print(len(pager))
+
+    pager = pager[len(pager) - 1].text
+    if pager == 'next':
+        print("Texte du next : ", pager)
+
+    else:
+        print("Il n'y a pas une deuxième page")
 
 else:
     print("Il y a un problème de request")
+
